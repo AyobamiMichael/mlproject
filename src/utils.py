1991b16d07abdce1a2a3f5c910133d2aea335f11
Ayobami <ayobamiwealth@gmail.com>
2025-09-11 00:29:00 +0100

    Model Trianer

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+from sklearn.metrics import r2_score
+
 
 from src.exception import CustomException
 import dill
@@ -19,5 +21,30 @@ def save_object(file_path, obj):
 
 
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train,y_train, X_test, y_test, models):
+    try: 
+       # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        report = {}
+
+        for model_name, model in models.items():
+            #models = list(models.values())[i]
+            
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            y_train_pred = model.predict(X_train)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+
+        return report
     except Exception as e:
         raise CustomException(e, sys)
